482de67c0bbc10426f8341fc66f0b62d2ba62539
Rizky Ramadhan <rizkyrmdhn09@gmail.com>
2023-11-06 11:04:48 +0800

    fix migration

--- database/migrations/2023_10_21_094031_sub_kriterias_table.php
+++ database/migrations/2023_10_21_094031_sub_kriterias_table.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
- 
-Schema::table('sub_kriterias', function (Blueprint $table) {
-    $table->boolean('is_header')->nullable()->default(false)->change();
-});
+use Illuminate\Database\Migrations\Migration;
+
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sub_kriterias', function (Blueprint $table) {
+            $table->boolean('is_header')->nullable()->default(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sub_kriterias');
+    }
+};
--- database/migrations/2023_10_21_094050_atribut_kriterias_table.php
+++ database/migrations/2023_10_21_094050_atribut_kriterias_table.php
@@ -2,9 +2,28 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
- 
-Schema::table('atribut_kriterias', function (Blueprint $table) {
-    $table->unsignedBigInteger('id_kriteria')->nullable()->change();
-    $table->unsignedBigInteger('id_sub_kriteria')->nullable()->change();
-    $table->string('score')->nullable()->change();
-});
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('atribut_kriterias', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_kriteria')->nullable()->change();
+            $table->unsignedBigInteger('id_sub_kriteria')->nullable()->change();
+            $table->string('score')->nullable()->change();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('atribut_kriterias');
+    }
+};
